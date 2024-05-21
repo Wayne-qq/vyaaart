34be2e2eb4c321784d488eb4e65f77510abf98f2
Wayne-qq <153057848+Wayne-qq@users.noreply.github.com>
2024-05-21 10:57:51 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-//  display: none; для прелоадера через 3 секунди
+// Код для приховування прелоадера через 3 секунди
 window.addEventListener('load', function() {
     // Виконуємо через 3 секунди після завантаження сторінки
     setTimeout(function() {
@@ -34,20 +34,35 @@ window.addEventListener('load', function() {
         if (preloader) {
             // Додаємо клас 'hidden' щоб зробити його невидимим
             preloader.classList.add('hidden');
+            checkHiddenElements(); // Перевіряємо, чи обидва елементи приховані
         }
     }, 3000); // 3000 мілісекунд = 3 секунди
 });
-
-
-
 
 document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('#main__sub a');
     links.forEach(link => {
         link.addEventListener('click', function(event) {
             event.preventDefault(); // Запобігаємо переходу за посиланням
-            document.getElementById('main__sub').classList.add('hidden');
+            const sub = document.getElementById('main__sub');
+            if (sub) {
+                sub.classList.add('hidden');
+                checkHiddenElements(); // Перевіряємо, чи обидва елементи приховані
+            }
         });
     });
 });
+
+function checkHiddenElements() {
+    const preloader = document.querySelector('.main__preloader');
+    const sub = document.querySelector('#main__sub');
+    const menu = document.querySelector('.menu');
+
+    if (preloader && sub && menu) {
+        if (preloader.classList.contains('hidden') && sub.classList.contains('hidden')) {
+            menu.style.position = 'fixed';
+        }
+    }
+}
+
 
