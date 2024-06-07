9bd9eb1735e9b43bb3b05d4428750e6ff67c7ba3
Wayne-qq <153057848+Wayne-qq@users.noreply.github.com>
2024-06-07 19:48:52 +0300

    Update database.js

--- js/database.js
+++ js/database.js
@@ -7,7 +7,7 @@ export function initializeDatabase(db) {
     try {
       // Додавання документу з текстом "привіт гіт" у вашу колекцію "greetings"
       await addDoc(collection(db, "greetings"), {
-        message: "Привіт фаєр"
+        message: "Привіт фаєр бейс"
       });
 
       console.log("Дані успішно додані до бази даних");
