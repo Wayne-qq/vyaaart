8155b03c9e8ac9351f8f09a1ccb70387afe9b5bf
Wayne-qq <153057848+Wayne-qq@users.noreply.github.com>
2024-06-07 18:45:37 +0300

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1,33 +1,24 @@
-// import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// // Функція для ініціалізації бази даних
-// export function initializeDatabase(db) {
-//   // Функція для додавання даних до бази даних Firebase
-//   async function addToDatabase(name) {
-//     try {
- 
-//       await addDoc(collection(db, "greetings"), {
-//         message: "привіт база",
-//         name: name 
-//       });
+// Функція для ініціалізації бази даних
+export function initializeDatabase(db) {
+  // Функція для додавання даних до бази даних Firebase
+  async function addToDatabase() {
+    try {
+      // Додавання документу з текстом "привіт" у вашу колекцію "greetings"
+      await addDoc(collection(db, "greetings"), {
+        message: "привіт гітдва"
+      });
 
-//       console.log("Дані успішно додані до бази даних");
-//     } catch (error) {
-//       console.error("Помилка додавання даних до бази даних:", error);
-//     }
-//   }
+      console.log("Дані успішно додані до бази даних");
+    } catch (error) {
+      console.error("Помилка додавання даних до бази даних:", error);
+    }
+  }
 
-//   window.onload = function() {
-
-//     if (typeof name !== 'undefined') {
-//       addToDatabase(name); 
-//     } else {
-//       console.error("Змінна name не визначена");
-//     }
-//   };
-// }
-
-
+  // Виклик функції для додавання даних при завантаженні сторінки
+  window.onload = addToDatabase;
+}
 
 
 
